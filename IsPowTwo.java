7b747db877686c265db67cecd2947f7edcb8e96a
Priyotosh Roy <152520274+priyotosh-27@users.noreply.github.com>
2025-06-21 20:58:44 +0530

    Add files via upload

---
+++ IsPowTwo.java
@@ -0,0 +1,10 @@
+class IsPowTwo {
+    public static boolean isPowerofTwo(int n) {
+        return (n & n - 1) == 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(isPowerofTwo(n));
+    }
+}
